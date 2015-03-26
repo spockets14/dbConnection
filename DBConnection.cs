183965c876fc3aee380edcb53dc3aa677355aeb0
spockets14 <spockets14@hotmail.com>
2015-03-26 15:49:09 -0400

    First Commit

---
+++ DBConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+
+namespace DBConnection
+{
+    public partial class GetAConnection : IDisposable
+    {
+        string dbConnectionString = @"Data Source=database.db;version=3;";
+        //public SQLiteConnection getDBConnection()
+        public SQLiteConnection getDBConnection()
+        {
+            SQLiteConnection sqliteCon = new SQLiteConnection(dbConnectionString);
+            sqliteCon.Open();
+            return sqliteCon;
+        }
+    }
+}
